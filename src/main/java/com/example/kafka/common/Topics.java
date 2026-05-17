1ca088d880a287e46f01f24595fd893d43ab653c
danielgit0 <daniel.asistencia@gmail.com>
2026-05-17 10:39:25 +0200

    feat: create and configure topic with bean

--- src/main/java/com/example/kafka/common/Topics.java
+++ src/main/java/com/example/kafka/common/Topics.java
@@ -1,5 +1,23 @@
 package com.example.kafka.common;
 
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class Topics {
+
   public static final String EMPLOYEE_CREATED_V1 = "employee.local.kafka_demo.employee_created.v1";
+
+  @Bean
+  public NewTopic employeeCreateV1() {
+    return TopicBuilder.name(EMPLOYEE_CREATED_V1)
+        .partitions(3)
+        .replicas(3)
+        .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(TimeUnit.DAYS.toMillis(7)))
+        .build();
+  }
 }
