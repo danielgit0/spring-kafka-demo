37e643e796287d30503f62ec4b4ecfea7098e4ae
danielgit0 <daniel.asistencia@gmail.com>
2026-05-17 10:46:21 +0200

    refactor: rename api and remove unnecessary controller

--- src/main/java/com/example/kafka/producer/MessageApiImpl.java
+++ src/main/java/com/example/kafka/producer/MessageApiImpl.java
@@ -2,10 +2,9 @@ package com.example.kafka.producer;
 
 import static com.example.kafka.common.Topics.EMPLOYEE_CREATED_V1;
 
-import com.example.generated.api.EmployeeApi;
+import com.example.generated.api.MessageApi;
 import com.example.generated.kafka.Employee;
 import com.example.generated.model.EmployeeMessageRequest;
-import java.util.Optional;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,24 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.NativeWebRequest;
 
 @RestController
-public class EmployeeApiImpl implements EmployeeApi {
+public class MessageApiImpl implements MessageApi {
 
-  private static final Logger log = LoggerFactory.getLogger(EmployeeApiImpl.class);
+  private static final Logger log = LoggerFactory.getLogger(MessageApiImpl.class);
 
   private final KafkaTemplate<String, Employee> kafkaTemplate;
   private final ProducerCallback callback;
 
-  public EmployeeApiImpl(KafkaTemplate<String, Employee> kafkaTemplate, ProducerCallback callback) {
+  public MessageApiImpl(KafkaTemplate<String, Employee> kafkaTemplate, ProducerCallback callback) {
     this.kafkaTemplate = kafkaTemplate;
     this.callback = callback;
-  }
-
-  @Override
-  public Optional<NativeWebRequest> getRequest() {
-    return EmployeeApi.super.getRequest();
   }
 
   @Override
